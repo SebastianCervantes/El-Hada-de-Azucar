6ac7cad68b792dab934ebba9bc3f734a178708fd
SebastianCervantes <42254216+SebastianCervantes@users.noreply.github.com>
2018-11-09 14:04:31 -0800

    Final Commit Sebastian

--- js/jquery.js
+++ js/jquery.js
@@ -3,22 +3,7 @@
     //Blur
       $("#scroll-inicio").click(function(){
       $('html, body').animate({scrollTop: $('main.intro').offset().top }, 'slow');
-    return false;
+         return false;
       });
-
-      var scroll_pos = 0;
-            $(document).scroll(function() {
-                scroll_pos = $(this).scrollTop();
-                if(scroll_pos > 600) {
-                    $(".indx-nav a").css({"color":"#000"});
-                    $(".indx-nav").css("background-color", "#fff");
-                    //$("#home").attr("src", "../images/luna-negra.png");
-                    return true;
-                } else {
-                    $(".indx-nav a").css("color", "#fff");
-                    $(".indx-nav").css("background-color", "rgba(0,0,0,0)");
-                    //$("#home").attr("src", "../images/Luna-Logo-02.png");
-                }
-            });
 
     });
